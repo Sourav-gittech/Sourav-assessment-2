a69266b80e4408a350c3719b1b915f2e8c8cc958
Sourav Mondal <108756502+Sourav-gittech@users.noreply.github.com>
2025-08-06 16:45:27 +0530

    commited

--- src/api/axiosInstance/axiosInstance.js
+++ src/api/axiosInstance/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import baseUrl from "../api_url/api";
 
-const token = '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';
+const token = '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';
 
 const axiosInstance = axios.create({
     baseURL: baseUrl,
